83c0d365ebc08f8e7e2887a5c775ab10da36411d
jerethk <82040941+jerethk@users.noreply.github.com>
2022-06-07 00:43:49 +1000

    Add project files.

---
+++ AGLChallenge/Owner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AGLChallenge
+{
+    public class Owner
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public List<Pet> Pets { get; set; }
+
+        public Owner(string name, string gender, int age, List<Pet> pets)
+        {
+            this.Name = name;
+            this.Gender = gender;
+            this.Age = age;
+            this.Pets = pets;
+        }
+    }
+}
---
+++ AGLChallenge/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace AGLChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            bool isInputFailed = false;
+
+            using (StreamReader JsonReader = new StreamReader("people.json"))
+            {
+                try
+                {
+                    input = JsonReader.ReadToEnd();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    isInputFailed = true;
+                }
+            }
+
+            if (!isInputFailed)
+            {
+                List<Owner> ownerList = new List<Owner>();
+
+                try
+                {
+                    JsonDocument document = JsonDocument.Parse(input);
+
+                    foreach (JsonElement element in document.RootElement.EnumerateArray())
+                    {
+                        string name = element.GetProperty("name").GetString();
+
+                        Console.WriteLine(name);
+                    }
+                }
+                catch (JsonException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+            Console.WriteLine("Done");
+
+        }
+    }
+}
